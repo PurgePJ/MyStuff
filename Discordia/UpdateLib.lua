8cbefae13ef37c32a60b96540cabade03afd41cf
John Sandler <priciel@protonmail.com>
2016-11-29 18:39:34 +0100

    Create UpdateLib.lua

---
+++ Discordia/UpdateLib.lua
@@ -0,0 +1,34 @@
+local http1 = require('coro-http')
+
+local UpdateLib = {}
+
+local function WriteFile(file, text)
+		local file = io.open(file, "w")
+		file:write(text)
+		file:close()
+	end
+end
+
+local function Get(webVersion, dwload, fileLink)
+	coroutine.wrap(function()
+		if dwload == false then
+			local head, version = http1.request("GET", webVersion)
+			return version
+		else
+			assert(type(fileLink) == "string", "CompareVersionAsync <fileLink>: string expected.")
+			local _, bot = http1.request("GET", fileLink)
+			return bot
+		end
+	end)()
+end
+
+local function UpdateLib.CompareVersionAsync(localVersion, webVersion, fileLink)
+	if localVersion and webVersion then
+		if localVersion ~= Get(webVersion, false) then
+			local name = io.read()
+			WriteFile(name, Get(nil, true, fileLink))
+		end
+	end
+end
+
+return UpdateLib
