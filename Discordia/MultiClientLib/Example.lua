947416265e95a0e1d8378ad0174f365ea8d275f8
John Sandler <priciel@protonmail.com>
2016-12-05 21:46:41 +0100

    Create Example.lua

---
+++ Discordia/MultiClientLib/Example.lua
@@ -0,0 +1,48 @@
+local discordia = require('discordia')
+local MultiClient = require("MultiClient")
+
+discordia.loadOpus('libopus')
+discordia.loadSodium('libsodium')
+discordia.loadFFmpeg('ffmpeg')
+
+local client = discordia.Client()
+local voice = discordia.VoiceClient()
+
+
+client:on('ready', function()
+	printf('Logged in as %s', client.user.username)
+end)
+
+
+client:on('messageCreate', function(message)
+	if message.channel.id == "81402706320699392" then
+
+		local cmd, arg = string.match(message.content, '(%S+) (.*)')
+		cmd = cmd or message.content
+
+		local MultiClientGiver = MultiClient:VoiceClient(discordia, message.guild.name, client.guildCount)
+
+		if cmd == ".join" then
+
+			local success, data = client._api:getChannel(arg)
+
+			if success then
+				MultiClientGiver:joinChannel(client:getVoiceChannel(arg))
+				message.channel:sendMessage("Joined "..data.name.." channel.")
+			else
+				message.channel:sendMessage("Channel not found.")
+			end
+		end
+
+		if cmd == ".play" then
+			message.channel:sendMessage("Playing "..arg.." song.")
+
+			local stream = MultiClientGiver:createFFmpegStream(arg)
+			stream:play()
+		end
+	end
+
+end)
+
+
+client:run("Token")
