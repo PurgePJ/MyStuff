4178cdfa1bad071b51a5a233efa4340a861cb9b8
John Sandler <priciel@protonmail.com>
2016-12-06 14:30:13 +0100

    Update MultiClient.lua

--- Discordia/MultiClientLib/MultiClient.lua
+++ Discordia/MultiClientLib/MultiClient.lua
@@ -10,24 +10,24 @@ local function getReady(gCount, discordia)
 end
 
 function MultiClient:VoiceClient(discordia, gName, gCount, fn, bitrate) -- maybe guildId ?
-	fn = fn or p("Voice client succesfully used for "..gName.." guild.")
-	bitrate = bitrate or 96000
+	__fn__ = fn or p("Voice client succesfully used for "..gName.." guild.")
+	__bitrate__ = bitrate or 96000
 	if Started == 0 then
 		getReady(gCount, discordia)
 	end
 	for k, Container in pairs(clients) do
 		for nCheck, vClient in pairs(Container) do
 			if clients[k][3] == gName then
-				MultiClient:Event(vClient, fn)
-				vClient:setBitrate(bitrate)
+				MultiClient:Event(vClient, __fn__)
+				vClient:setBitrate(__bitrate__)
 				Started = 1
 				return vClient
 			else
 				if clients[k][2] == 0 then
 					clients[k][2] = 1
 					clients[k][3] = gName
-					MultiClient:Event(vClient, fn)
-					vClient:setBitrate(bitrate)
+					MultiClient:Event(vClient, __fn__)
+					vClient:setBitrate(__bitrate__)
 					Started = 1
 					return vClient
 				end
