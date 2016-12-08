a2a35edd1e5d65b06ddd8182aefc2c644040683e
John Sandler <priciel@protonmail.com>
2016-12-08 19:41:31 +0100

    Update Example.lua

--- Discordia/MultiClientLib/Example.lua
+++ Discordia/MultiClientLib/Example.lua
@@ -1,48 +1,50 @@
-local discordia = require('discordia')
+local discordia = require("discordia")
+_G.timer = require("timer")
 local MultiClient = require("MultiClient")
 
-discordia.loadOpus('libopus')
-discordia.loadSodium('libsodium')
-discordia.loadFFmpeg('ffmpeg')
-
-local client = discordia.Client()
-local voice = discordia.VoiceClient()
+local client = discordia:Client()
+local voice = discordia:VoiceClient()
 
 
-client:on('ready', function()
+client:on("ready", function() 
 	printf('Logged in as %s', client.user.username)
+	voice:loadOpus("libopus")
+	voice:loadSodium("libsodium")
 end)
-
 
 client:on('messageCreate', function(message)
 	if message.channel.id == "81402706320699392" and message.author.id == "191442101135867906" then
 
+		local voiceClient, connection = MultiClient:VoiceClient(discordia, message.guild.name, client.guildCount)
 		local cmd, arg = string.match(message.content, '(%S+) (.*)')
 		cmd = cmd or message.content
 
-		local MultiClientGiver = MultiClient:VoiceClient(discordia, message.guild.name, client.guildCount)
+		if cmd == ".bitrate" then
+			connection:setBitrate(arg) -- We use current connection to change it's bitrate
+			message.channel:sendMessage("Bitrate changed to "..arg..".")
+		end
 
 		if cmd == ".join" then
-
 			local success, data = client._api:getChannel(arg)
 
 			if success then
-				MultiClientGiver:joinChannel(client:getVoiceChannel(arg))
+				voiceClient:joinChannel(client:getVoiceChannel(arg)) -- But we use our VoiceClient to connect to a channel.
 				message.channel:sendMessage("Joined "..data.name.." channel.")
 			else
 				message.channel:sendMessage("Channel not found.")
 			end
 		end
 
+
 		if cmd == ".play" then
 			message.channel:sendMessage("Playing "..arg.." song.")
 			client:setGameName("Playing "..arg.." song.")
-			local stream = MultiClientGiver:createFFmpegStream(arg)
-			stream:play()
+			connection:playFile("songs/"..arg) -- On the other hand, we use connection to send our music.
 		end
+
 	end
 
 end)
 
 
-client:run("Token")
+client:run('Token')
