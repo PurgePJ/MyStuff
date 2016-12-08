b515a7dcb9185d503e4851e9c8b69163ffba5dd5
John Sandler <priciel@protonmail.com>
2016-12-08 19:41:46 +0100

    Update MultiClient.lua

--- Discordia/MultiClientLib/MultiClient.lua
+++ Discordia/MultiClientLib/MultiClient.lua
@@ -1,45 +1,42 @@
 local MultiClient, clients = {}, {}
-
-local Started = 0
+local Started, con = 0, nil
 
 local function getReady(gCount, discordia)
 	for i =  1, gCount do
-		print("Adding voice client number: " .. i)
+		print("Adding voice client number: ".. i)
 		clients[i] = {discordia.VoiceClient(), 0, ""}
 	end
 end
 
-function MultiClient:VoiceClient(discordia, gName, gCount, bitrate) -- maybe guildId ?
-	__bitrate__ = bitrate or 96000
-	_G.gName = gName
+local function Ready(gCount, discordia)
 	if Started == 0 then
 		getReady(gCount, discordia)
-	end
-	for k, Container in pairs(clients) do
-		for nCheck, vClient in pairs(Container) do
-			if clients[k][3] == gName then
-				vClient:setBitrate(__bitrate__)
-				Started = 1
-				return vClient
-			else
-				if clients[k][2] == 0 then
-					clients[k][2], clients[k][3], Started = 1, gName, 1
-					vClient:setBitrate(__bitrate__)
-					return vClient
-				end
-			end
-		end
+		Started = 1
 	end
 end
 
-function MultiClient:Event(instance, fn)
-	instance:on('connect', function()
-		if fn then
-			pcall(fn)
-		else
-			p("Voice client succesfully used for ".. gName .. " guild.")
-		end
+local function EmulateEvent(client)
+	client:on("connect", function(connection)
+		print(connection)
+		con = connection
 	end)
+	return con
+end
+
+function MultiClient:VoiceClient(discordia, gName, gCount, bitrate) -- maybe guildId ?
+	Ready(gCount, discordia)
+	for k, Container in pairs(clients) do
+		for nCheck, vClient in pairs(Container) do
+			if clients[k][3] == gName then
+				return vClient, EmulateEvent(vClient)
+			elseif clients[k][2] == 0 then
+				clients[k][2], clients[k][3] = 1, gName
+				local connection = EmulateEvent(vClient)
+				timer.sleep(1000)
+				return vClient, connection
+			end
+		end
+	end
 end
 
 
